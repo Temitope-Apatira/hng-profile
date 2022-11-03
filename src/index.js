9ae8e88e5cb36dd2d7dae91888989b9621c8cb88
Apatira Temitope <83637849+Temitope-Apatira@users.noreply.github.com>
2022-11-03 19:18:46 +0100

    fix: deploy netlify

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
